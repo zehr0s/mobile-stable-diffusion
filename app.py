5c108fb6bbb0a80a71cad88992e07b2cbed9d8a5
zehr0s <zehr0.s@protonmail.com>
2023-10-14 13:46:03 -0600

    Mobile script debugging

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Imports
 import torch
 import datetime, os
 from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler, EulerDiscreteScheduler
@@ -16,8 +17,8 @@ pipeline = DiffusionPipeline.from_pretrained(
     low_cpu_mem_usage=True,
     torch_dtype=torch.float32 # torch.float16
 )
-pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
-# pipeline.scheduler = EulerDiscreteScheduler.from_config(pipeline.scheduler.config)
+# pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
+pipeline.scheduler = EulerDiscreteScheduler.from_config(pipeline.scheduler.config)
 
 # pipeline = pipeline.to("cuda")
 
@@ -25,14 +26,13 @@ pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.
 pipeline.safety_checker = lambda images, clip_input: (images, False)
 
 # Some settings
-mobile = False
+mobile = True
 portrait = True
-res = [360, 640] if mobile else [512, 768]
+res = [320, 512] if mobile else [512, 768]
 res = sorted(res, reverse=True) if portrait else sorted(res)
-res = [320, 320]
 
 # Prompt engineering
-prompt = "(masterpiece, best quality, high quality, highres:1.4), ambient soft lighting, 4K, 1girl, cute, huge breasts,, close-up,long hair,pink hair,hairband,((gradient hair))"
+prompt = "(masterpiece, best quality, high quality, highres:1.4), ambient soft lighting, 4K, 1girl, cute, huge breasts,close-up,long hair,pink hair,hairband,((gradient hair))"
 
 negative = "easynegative, badhandv4, (worst quality, low quality, normal quality), bad-artist, blurry, ugly, ((bad anatomy)),((bad hands)),((bad proportions)),((duplicate limbs)),((fused limbs)),((interlocking fingers)),((poorly drawn face)),clothes,logo,watermark,muscles:1.3,elf,elf ears,"
 
@@ -47,33 +47,16 @@ with torch.no_grad():
         negative_prompt =negative,
         height=res[0],
         width=res[1],
-        num_inference_steps=32,
+        num_inference_steps=26,
         guidance_scale=8,
         max_embeddings_multiples = 8,
     ).images[0]
 
 # Save image
-out_path = "."
-# out_path = "/sdcard/Download"
+out_path = "./Outputs"
+mobile_path = "/sdcard/Download"
+if os.path.isdir(mobile_path):
+    out_path = mobile_path
 out_name = f"{model_name}_{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.png"
 out_file = os.path.join(out_path, out_name)
 img.save(out_file)
-
-### Deprecated
-# Use Euler sampling
-# scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-
-# Enable low memory usage, set image size to 320x320 and steps to 50.
-# pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, low_cpu_mem_usage=True, height=320, width=320, num_inference_steps=30)
-
-# Bypass NSFW filter
-# pipe.safety_checker = lambda images, clip_input: (images, False)
-
-# Prompts
-# prompt = "A victorian woman stands on grass field."
-
-# Start generating the image. Use CPU only
-# image = pipe(prompt).images[0]
-
-# Save the image
-# image.save("result.png")
